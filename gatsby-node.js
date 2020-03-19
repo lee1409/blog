8184ab3ee2d19f6b690dd1567f9c341f68f44876
lee1409 <leerun1409@gmail.com>
2020-03-19 16:13:13 +0800

    create dynamic route

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+// gatsby-node.js
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/post/)) {
+    page.matchPath = "/post/*"
+
+    // Update the page.
+    createPage(page)
+  }
+}
