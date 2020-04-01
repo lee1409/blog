f50fcd876779c4fb394c1260f5ca0607f9fe46fc
lee1409 <leerun1409@gmail.com>
2020-04-01 14:54:33 +0800

    added header into post

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../components/layout/layout"
-import "../static/screen.css";
-import { Router } from "@reach/router";
+import "../static/screen.css"
+import { Router } from "@reach/router"
 import api from "../ghost"
 import { CircularProgress } from "@material-ui/core"
 import SEO from "../components/seo"
+import Header from "../components/header/header"
+import HomeIcon from '@material-ui/icons/Home'
+import {navigate} from "gatsby";
 
-const Content = ({slug}) => {
-  const [data, setData] = useState();
+
+const Content = ({ slug }) => {
+  const [data, setData] = useState()
 
   useEffect(() => {
-    api.posts.read({slug: slug}, {formats: ['html', 'plaintext']}).then((resp) => setData(resp));
-  }, []);
+    api.posts.read({ slug: slug }, { formats: ["html", "plaintext"] }).then((resp) => setData(resp))
+  }, [])
 
   if (data) {
-    console.log(JSON.stringify(data));
-    return <>
-      <SEO
-        title={data.title}
-        description={data.meta_description}
-        meta={[
-          {name: "canonical_url", content: data.canonical_url},
-          ]} />
-      <h1>{data.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.html}} /></>
+    return (
+      <>
+        <SEO
+          title={data.title}
+          description={data.meta_description}
+          meta={[
+            { name: "canonical_url", content: data.canonical_url },
+          ]}/>
+        <Header
+          title={"Notetaking"}
+          onClick={() => navigate('/')}/>
+          <section className={'content'}>
+            <h4 className={'content-title'}>{data.title}</h4>
+            <div dangerouslySetInnerHTML={{ __html: data.html }}/>
+          </section>
+
+
+
+      </>
+      )
   }
   return <CircularProgress/>
 }
